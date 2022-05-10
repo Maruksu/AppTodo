41a1b92a05a4b70372438f081abf4a9fe3dbb109
Marcos Marcelino Assis de Souza <marcosmarcelinoassis@gmail.com>
2022-05-10 19:47:37 -0400

    aaaaaaa

--- AppTodo/src/app/home/home.page.ts
+++ AppTodo/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController, ToastController } from '@ionic/angular';
+import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +8,10 @@ import { AlertController, ToastController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController) {}
+  
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController, private actionSheetCtrl: ActionSheetController) {}
+
+  tarefas: any[] = [];
 
   async showAdd() {
     const alert = await this.alertCtrl.create({
@@ -48,10 +51,56 @@ export class HomePage {
         message: 'Digite a tarefa',
         duration: 2000,
         position: 'top',
+      
       });
-
+    
       toast.present();
       return;
     }
+    const tarefa = {nome:novaTarefa, realizada: false};
+    this.tarefas.push(tarefa);
+    this.salvaLocalStorage();
+
   }
+  salvaLocalStorage(){
+    localStorage.setItem('tarefaUsuario', JSON.stringify(this.tarefas));
+
+    let tarefaSalva = localStorage.getItem('tarefaUsuario');
+
+      if (tarefaSalva != null) {
+        this.tarefas = JSON.parse(tarefaSalva);
+      }
+    
+  }
+  async realizaAcoes(tarefa: any) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Qual ação realizar?',
+      buttons: [{
+        text: tarefa.realizada ? 'Desmarcar' : 'Marcar',
+        icon: tarefa.realizada ? 'checkmark-circle' : 'radio-button-off-outline',
+        handler: () => {
+          tarefa.realizada = !tarefa.realizada;
+          this.salvaLocalStorage();
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const {role, data} = await actionSheet.onDidDismiss();
+
+  }
+  excluirTarefa(tarefa: any) {
+    this.tarefas = this.tarefas.filter(arrayTarefa => tarefa != arrayTarefa);
+
+    this.salvaLocalStorage();
+  }
+
+  
 }
